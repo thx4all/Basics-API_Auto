36563ca1e737fc75f13f87cc94fdb66672eef1ed
Kefir <Kefir@DESKTOP-ILGTVI8>
2019-02-11 22:08:52 +0300

    add methods and class for get them

---
+++ Basics/src/Methods1.java
@@ -0,0 +1,21 @@
+
+public class Methods1 {
+
+	public void PrintSometh()
+	{
+		System.out.println("I can print anything here :P");
+	}
+		
+	
+	public String ReturnedStr()
+	{
+		return "Hello, i am a string";
+	}
+		
+		
+	public int ReturnedInt()
+	{
+		return 2;
+	}
+
+}
---
+++ Basics/src/Try4_Syntax.java
@@ -0,0 +1,24 @@
+
+
+public class Try4_Syntax {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//check github
+
+	
+	int a=3;
+	int b=4;
+	int AB=a+b;
+	System.out.println("SUM 3 and 4 equal to "+AB+" >:3");
+	
+	Methods1 m=new Methods1();
+	m.PrintSometh();
+	
+	String rs=m.ReturnedStr();
+	System.out.println(rs);
+	
+	Integer ri=m.ReturnedInt();
+	System.out.println("And i am a integer => "+ri);
+}
+}
